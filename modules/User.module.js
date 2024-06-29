13f14a1a5da3607be003ff0f94c55beb3bb96d7a
unknown <sharma.harshit295@gmail.com>
2024-06-29 10:13:03 +0530

    cors added

--- modules/User.module.js
+++ modules/User.module.js
@@ -1,15 +1,20 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const userschema=mongoose.Schema({
-    name:{type:String,require:true},
-    email:{type:String,require:true,unique:true},
-    password:{type:String,require:true},
-    age:{type:Number,require:true},
-    role:{type:Array,require:true},
-    userId:{type:String,require:true}
-},{
-    versionKey:false,
-    timestamps: true
-})
-const usermodel=mongoose.model("userdata",userschema)
-module.exports=usermodel
+const userSchema = mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        age: { type: Number, required: true },
+        role: { type: Array, required: true },
+        userId: { type: String, required: true }
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+);
+
+const UserModel = mongoose.model("UserData", userSchema);
+
+module.exports = UserModel;
